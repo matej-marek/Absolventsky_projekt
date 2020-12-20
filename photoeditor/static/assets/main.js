b7bd94f83db58a82cf8e86be6e98bbe4e9fa36bb
matej-marek <72819092+matej-marek@users.noreply.github.com>
2020-12-20 11:22:11 +0100

    drobné úpravy

--- photoeditor/static/assets/main.js
+++ photoeditor/static/assets/main.js
@@ -28,6 +28,7 @@ $(".settingHide").on("click",function(){
    $(".settingBackground").hide()
 });
 /*show infotable */ 
+$(".uploadProjectBackground").hide()
 $(".open").on("click",function(){
   $(".uploadProjectBackground").show();
   var template=$("#zalozkaTemplate").html();
@@ -37,10 +38,40 @@ $(".open").on("click",function(){
 });
 /* hide infotable */ 
 $(".uploadProjectHide").on("click",function(){
-  $(".uploadProjectBackground").hide()
+  $(".uploadProjectBackground").hide();
 });
 /* fisrt hide */ 
-$(".uploadProjectBackground").hide()
+$(".windowsBackground").hide()
+$(".windows").on("click",function(){
+  $(".windowsBackground").show();
+  var template=$("#zalozkaTemplate").html();
+  if(zalozky.children(".itemZalozky").attr("data-name")=="windows"){
+      $(zalozky.children(".itemZalozky").attr("data-name")).remove();
+  }
+});
+/* hide infotable */ 
+$(".windowsHide").on("click",function(){
+  $(".windowsBackground").hide();
+});
+/* fisrt hide */ 
+$(".windowsBackground").hide();
+
+
+
+/* fisrt hide */ 
+$(".toolsBackground").hide();
+$(".tool").on("click",function(){
+  $(".toolsBackground").show();
+  var template=$("#zalozkaTemplate").html();
+  if(zalozky.children(".itemZalozky").attr("data-name")=="tools"){
+      $(zalozky.children(".itemZalozky").attr("data-name")).remove();
+  }
+});
+/* hide infotable */ 
+$(".toolsHide").on("click",function(){
+  $(".toolsBackground").hide()  
+});
+/* fisrt hide */ 
 /* skrytí pravého kliknutín a shift ctrl i a F12 */
 $(window).on('keydown',function(event){
     if(location.hostname == "photoeditor.matejmarek.cz"){
@@ -53,7 +84,7 @@ $(window).on('keydown',function(event){
         return false;  //Prevent from ctrl+shift+i
       }
       else if(event.ctrlKey && event.keyCode==73){
-        alert('Entered ctrl+shift+i')
+        alert('Entered ctrl+i')
         return false;  //Prevent from ctrl+shift+i
       }
     }
